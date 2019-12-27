70ade2ee89d2acd2c0a0fac5c22d3d11cc8a104d
cposca <cposca3@gmail.com>
2019-12-27 00:12:33 +0000

    Updates

---
+++ precache-manifest.77dec8e5538f73c9cbc7bbb74b20f615.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e0656a0a941ec7ec03d52c94224e58",
+    "url": "/cposca.github.io/index.html"
+  },
+  {
+    "revision": "3da70f06aa53ccd8dd70",
+    "url": "/cposca.github.io/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "567805f339c9279e0f82",
+    "url": "/cposca.github.io/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3da70f06aa53ccd8dd70",
+    "url": "/cposca.github.io/static/js/2.bdf43cc1.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/cposca.github.io/static/js/2.bdf43cc1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "567805f339c9279e0f82",
+    "url": "/cposca.github.io/static/js/main.63ea64f3.chunk.js"
+  },
+  {
+    "revision": "732bc146b8c5ec52aabd",
+    "url": "/cposca.github.io/static/js/runtime-main.b0732e70.js"
+  },
+  {
+    "revision": "07df0f9127f7b24561752e809e43786d",
+    "url": "/cposca.github.io/static/media/avatar.07df0f91.png"
+  },
+  {
+    "revision": "b3f8a2ee4eae13fe6fe3dbac4154db15",
+    "url": "/cposca.github.io/static/media/background.b3f8a2ee.png"
+  }
+]);
